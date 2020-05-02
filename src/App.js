0037431fd8db8f709fe6ea85f02b062ba170dd9c
eshoiza <engrjayt200@gmail.com>
2020-05-02 17:33:14 +0100

    added a changeNameHandler

--- src/App.js
+++ src/App.js
@@ -1,82 +1,71 @@
 import React, { Component } from 'react'
-import Validation from './Validation/Validation';
-import Char from './Char/Char';
-
 import './App.css';
+import Radium, {StyleRoot } from 'radium';
+import Person from './Person/Person';
+class App extends Component {
+  state = {
+    persons: [
+      { id: "asfa1", name: "Kelvin", age: 30 },
+      { id: "vasdf1", name: "Simon", age: 21 },
+      { id: "asdf11", name: "Stephanie", age: 26 },
+    ],
+    otherState: 'some value here',
+    showPerson: false
+  };
 
-export default class App extends Component {
+  nameChangeHandler = (e, id)=>{
+    const personIndex = this.state.persons.findIndex(p=>{
+      return p.id === id;
+    });
 
-state = {
-    userInput: '', 
-    name : 'James Oyanna'
+   const person  = {
+     ...this.state.person[personIndex]
+   };
+   person.name = e.target.value;
+   const persons = [...this.state.persons]
+   persons[personIndex] = person;
+  this.setState({persons:persons});
   }
-  
-    inputChangeHandler = (e)=>{
-      this.setState({ userInput: e.target.value });
-    }
 
-    deleteCharHandler = (index)=>{
-    const text = this.state.userInput.split('');
-    text.splice(index, 1)
-    const updatedText = text.join('');
-    this.setState({userInput: updatedText})
-    }
+  togglePersonHandler = ()=>{
+    const doesShow = this.state.showPerson;
+    this.setState({showPerson: !doesShow})
+  }
 
   render() {
+  const style = {
+    backgroundColor: 'green',
+    color: 'white',
+    font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+  }
+  };
 
-    const charList = this.state.userInput.split('').map((ch, index)=>{
-      return <Char 
-      character={ch}
-       key={index} 
-       clicked={()=>this.deleteCharHandler(index)} />;
-    })
-
-    let classes = ['red', 'bold'].join(' ');
-    return (
-      <div className="App">
-        <p className={classes}>You mus turn to red</p>
-        <ol>
-          <li>
-            Create an input field (in App component) with a change listener
-            which outputs the length of the entered text below it (e.g. in a
-            paragraph).
-          </li>
-          <li>
-            Create a new component (=> ValidationComponent) which receives the
-            text length as a prop
-          </li>
-          <li>
-            Inside the ValidationComponent, either output "Text too short" or
-            "Text long enough" depending on the text length (e.g. take 5 as a
-            minimum length)
-          </li>
-          <li>
-            Create another component (=> CharComponent) and style it as an
-            inline box (=> display: inline-block, padding: 16px, text-align:
-            center, margin: 16px, border: 1px solid black).
-          </li>
-          <li>
-            Render a list of CharComponents where each CharComponent receives a
-            different letter of the entered text (in the initial input field) as
-            a prop.
-          </li>
-          <li>
-            When you click a CharComponent, it should be removed from the
-            entered text.
-          </li>
-        </ol>
-        <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
-        <input
-          type="text"
-          onChange={this.inputChangeHandler}
-          value={this.state.userInput}
-        />
-        <p>{this.state.userInput}</p>
-        <Validation 
-        inputLength={this.state.userInput.length} />
-        {charList}
+  let persons = null;
+  if(this.state.showPerson){
+    persons = (
+      <div>
+        {this.state.persons.map((person, index)=>{
+          return <Person
+          click = {()=>this.deletePersonHandler(index)}
+          name = {person.name}
+          age = {person.age}
+          key = {person.id}
+          change = {(e)=>this.nameChangeHandler(e, person.id)} 
+          />
+        
+        })};
       </div>
-    );
+    )
+  }
+
+    return <div></div>;
   }
 }
 
+export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Radium from 'radium';
+import './Person.css';
+
+const Person = (props)=>{
+    const style = {
+        BackgroundColor: 'brown',
+        width: '450px'
+    }
+    return(
+        <div className="Person" style={style}>
+            <p onClick={props.click}>I am {props.name} and I am {props.age} years old</p>
+        <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
+}
+
+export default Radium(Person);
