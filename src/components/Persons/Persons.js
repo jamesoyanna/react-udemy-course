b187fae5a269bc35d7786d7f28da06dbe96697e8
eshoiza <engrjayt200@gmail.com>
2020-05-04 23:21:49 +0100

    updated lifecycle methods

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,6 +2,26 @@ import React, {Component} from 'react';
 import Person from '../Person/Person';
 
 class Persons extends Component{
+  // static getDerivedStateFromProps(props, state){
+  //   console.log('[Persons js] getderivedStateFromProps')
+  //   return state
+  // }
+shouldComponentUpdate(nextProps, nextState){
+  console.log('[Persons.js] shouldcomponentUpdate')
+  return false
+}
+
+getSnapshotBeforeUpdate(prevProps, prevState){
+  console.log('[Persons.js] getSnapshotBeforeUpdate')
+  return {message: 'Snapshot'}
+}
+
+componentDidUpdate(prevProps, prevState, snapshot){
+  console.log('[persons.js] componentDidUpdate');
+  console.log(snapshot);
+}
+
+
   render(){
     console.log("[Persons js] rendering");
   return this.props.persons.map((person, index) => {
