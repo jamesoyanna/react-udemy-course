8e114208749ec97144424a8437c922e6061f8d54
eshoiza <engrjayt200@gmail.com>
2020-05-04 23:42:01 +0100

    updated cockpit component

--- src/components/CockPit/CockPit.js
+++ src/components/CockPit/CockPit.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import classes from './Cockpit.css';
+//import useEffect from 'react';
 
 const Cockpit = (props)=>{
+
+  useEffect(()=>{
+    console.log('cockpit.js useeffect')
+  })
   const assignedClasses = [];
   let btnClass = "";
   if(props.showPersons){
