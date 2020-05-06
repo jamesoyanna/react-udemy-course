b2f825f00a82591965f4ebfee286772b78894692
eshoiza <engrjayt200@gmail.com>
2020-05-06 02:41:19 +0100

    setting state correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -20,7 +20,8 @@ class App extends Component {
     otherState: "some other value",
     showPersons: false,
     showCockpit: true,
-    man: 'James'
+    changeCounter: 0
+
   };
 
   static getDerivedStateFromProps(props, state){
@@ -49,7 +50,12 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState((prevState,props)=>{
+     return {
+       persons: persons,
+      changeCounter: this.state.changeCounter+1 
+    };
+    });
   };
 
   deletePersonHandler = (personIndex) => {
